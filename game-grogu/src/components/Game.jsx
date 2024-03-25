ef8878e8836aaa073f2396882068dcfdd5efe901
victoriagz <victoriagabrielagonzalez123@hotmail.com>
2024-03-25 11:22:57 +0100

    router

--- game-grogu/src/components/Game.jsx
+++ game-grogu/src/components/Game.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function Game() {
-  return <div>Game</div>;
+  return (
+    <div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui quia,
+        saepe, fuga expedita ad illum, alias rem deleniti nemo aspernatur
+        voluptas commodi aperiam soluta veniam earum? Adipisci molestias tempore
+        laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+        repellendus, repudiandae iste ipsa alias adipisci? Ex similique nobis
+        hic laboriosam aut consequatur ut praesentium, asperiores esse. Nesciunt
+        rerum libero natus. Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Consequatur sint esse laudantium optio sapiente dolores nisi
+        deleniti aliquid labore beatae. Ullam corporis excepturi quod nihil
+        deleniti iure illo impedit quis.
+      </p>
+      <Link to="/">Volver al Inicio</Link>
+    </div>
+  );
 }
 
 export default Game;
